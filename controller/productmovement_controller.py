59f4b95c61469a9682cc4ab54b7bc0c578831ef1
Dhrumil Pandya <dvpandya2138433@gmail.com>
2023-08-18 00:25:18 +0530

    chore: add controller for product-movement

---
+++ controller/productmovement_controller.py
@@ -0,0 +1,15 @@
+from models.product import Product
+from models.location import Location
+from models.product_movement import ProductMovement
+from flask import render_template, request
+from app import db,app
+from services.productmovement_service import productmoevementservice
+
+@app.route('/product_movement', methods=['GET', 'POST'])
+def get_product_movement():
+    products = productmoevementservice.get_all_products()
+    locations = productmoevementservice.get_all_locations()
+    productmoveements = productmoevementservice.get_product_movements()
+    return render_template('product_movement_form.html', movement=None,
+                           products=products, locations=locations, productMovements = productmoveements)
+
